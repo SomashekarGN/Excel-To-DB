faddc6b5c97ef899f6027f34cf9fe97baf5e87bd
SomashekarGN <somashekar.xworkz@gmail.com>
2025-02-05 22:55:56 +0530

    Initial Commit

---
+++ Excel-to-DB/src/main/java/com/xworkz/projectexcel/entity/ExcelDTO.java
@@ -0,0 +1,16 @@
+package com.xworkz.projectexcel.entity;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ExcelDTO {
+}
